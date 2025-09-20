f62bffbd3a4f4749fb5f3f730816deaa14a0f33a
Arin Das <iamarindas@gmail.com>
2025-09-20 12:00:15 +0530

    Complete qn 6

---
+++ Ass3/qn6.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+struct Student
+{
+    int roll;
+    float score;
+
+    void inputDetails(){
+        cout<<"Enter roll: "; cin>>this->roll;
+        cout<<"Enter score: "; cin>>this->score;
+    }
+
+    void showDetails(){
+        cout<<"Student with roll: "<<roll<<" has score: "<<score<<endl;
+    }
+
+    friend void update(Student &);
+    friend void displayUpdated(Student &);
+};
+
+void update(Student &stu){
+    cout<<"\nEnter new score: "; cin>>stu.score;
+}
+
+void displayUpdated(Student &stu){
+    cout<<"Student with roll: "<<stu.roll<<" now has score: "<<stu.score<<endl;
+}
+
+int main(){
+    Student stu;
+
+    stu.inputDetails();
+    stu.showDetails();
+
+    update(stu);
+    displayUpdated(stu);
+
+    return 0;
+}
